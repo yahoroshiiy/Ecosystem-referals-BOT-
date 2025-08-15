a4f577b5dd6e5aa30b8be9ffcb42af2a37e99f36
yahoroshiiy <pprekrasenie@gmail.com>
2025-08-15 03:05:20 +0200

    nubezgovna

--- Новая папка/handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from handlers import client
-from handlers import admin
-from handlers import fsm
-from handlers import db
--- Новая папка/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call %~dp0ukrainegta_help_bot\venv\Scripts\activate
-cd %~dp0ukrainegta_help_bot
-python main.py
-pause
